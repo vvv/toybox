b8af335db7e4f657a016713ef774ae884b1a1244
Valeriy V. Vorotyntsev <valery.vv@gmail.com>
2022-03-30 16:31:17 +0300

    ctime: Minor decorative edit

--- ctime/src/main.rs
+++ ctime/src/main.rs
@@ -2,24 +2,6 @@ use chrono::{DateTime, Local, TimeZone};
 use std::{os::unix::fs::MetadataExt, time::SystemTime};
 use walkdir::WalkDir;
 
-// https://users.rust-lang.org/t/convert-std-time-systemtime-to-chrono-datetime-datetime/7684/4
-fn _system_time_to_date_time(t: SystemTime) -> DateTime<Local> {
-    let (sec, nsec) = match t.duration_since(SystemTime::UNIX_EPOCH) {
-        Ok(dur) => (dur.as_secs() as i64, dur.subsec_nanos()),
-        Err(e) => {
-            // unlikely but should be handled
-            let dur = e.duration();
-            let (sec, nsec) = (dur.as_secs() as i64, dur.subsec_nanos());
-            if nsec == 0 {
-                (-sec, 0)
-            } else {
-                (-sec - 1, 1_000_000_000 - nsec)
-            }
-        }
-    };
-    Local.timestamp(sec, nsec)
-}
-
 fn main() {
     for entry in WalkDir::new("src")
         .into_iter()
@@ -63,4 +45,22 @@ fn main() {
         let mtime_dt_ext = Local.timestamp(meta.mtime(), meta.mtime_nsec() as _);
         assert_eq!(mtime_dt, mtime_dt_ext);
     }
+}
+
+// https://users.rust-lang.org/t/convert-std-time-systemtime-to-chrono-datetime-datetime/7684/4
+fn _system_time_to_date_time(t: SystemTime) -> DateTime<Local> {
+    let (sec, nsec) = match t.duration_since(SystemTime::UNIX_EPOCH) {
+        Ok(dur) => (dur.as_secs() as i64, dur.subsec_nanos()),
+        Err(e) => {
+            // unlikely but should be handled
+            let dur = e.duration();
+            let (sec, nsec) = (dur.as_secs() as i64, dur.subsec_nanos());
+            if nsec == 0 {
+                (-sec, 0)
+            } else {
+                (-sec - 1, 1_000_000_000 - nsec)
+            }
+        }
+    };
+    Local.timestamp(sec, nsec)
 }
